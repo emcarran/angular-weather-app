f2c89ee1f0c8bf7e2ffc274fb751b02d88db6e11
Elizabeth Carranza <elizabeth.m.carranza@gmail.com>
2016-08-15 17:28:33 -0700

    up and working

--- app/owm-library.js
+++ app/owm-library.js
@@ -1,8 +1,10 @@
 angular.module('owmLibrary', [])
 
+//each constant below defines the API structure, which includes the URL base, the API key and a variable containing the path to the local JSON file (which contains a list of cities)
 .constant('OWM_API_PREFIX', 'http://api.openweathermap.org/data/2.5/forecast')
     .constant('OWM_API_KEY', 'db886594d5161ed75ebbfbcda76ccff4')
     .constant('OWM_CITIES_JSON_FILE', './owm-cities.json')
+    //key service for the entire app
     .factory('owmRequest', ['$http', '$q', 'OWM_API_PREFIX', 'OWM_API_KEY', function ($http, $q, OWM_API_PREFIX, OWM_API_KEY) {
         return function (params) {
             var reqParams = angular.extend({}, params, {
